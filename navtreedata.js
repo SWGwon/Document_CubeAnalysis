4eba669bdf1c2697f9daa637d3e97fe915b586d0
SunWoo Gwon <tnsdn302@naver.com>
2020-12-26 00:06:59 +0900

    update

--- navtreedata.js
+++ navtreedata.js
@@ -24,7 +24,7 @@
 */
 var NAVTREE =
 [
-  [ "My Project", "index.html", [
+  [ "CubeAnalysis", "index.html", [
     [ "Classes", "annotated.html", [
       [ "Class List", "annotated.html", "annotated_dup" ],
       [ "Class Index", "classes.html", null ],
